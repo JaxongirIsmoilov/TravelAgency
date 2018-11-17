ffc819085ce902e7f7e73a2851f5b312a9374e9e
behzodjon <khudoyberdievgates@gmail.com>
2018-11-18 02:01:52 +0500

    controllers

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get ('/index', ['uses' => 'MainController@show']);
+Route::get ('/blog', ['uses' => 'MainController@show2']);
